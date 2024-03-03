f221e81245079f16a16163f4fd22d1af4e4dc54b
Shua317 <157175620+Shua317@users.noreply.github.com>
2024-03-04 04:08:15 +0800

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,116 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Upon entering Teyvet, what did you fish out first?",
+    answer: "Paimon",
+    options: [
+      "Emergency Food",
+      "Fish",
+      "Paimon",
+      "Silly Mascot"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which continent did you first enter at the start of the game?",
+    answer: "Mondstadt",
+    options: [
+      "Liyue",
+      "Inazuma",
+      "Mondstadt",
+      "Fontaine"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Who’s the oldest Archon?",
+    answer: "Zhongli",
+    options: [
+      "Venti",
+      "Zhongli",
+      "Ei",
+      "Nahida"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What reaction happens if you attack with Cryo and Pyro?",
+    answer: "Melt",
+    options: [
+      "Freeze",
+      "Thaw",
+      "Melt",
+      "Overload"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What reaction happens if you attack with Dendro and Hydro?",
+    answer: "Bloom",
+    options: [
+      "Bloom",
+      "Growth",
+      "Burn",
+      "Flourish"
+    ]
+  },
+    {
+    numb: 6,
+    question: "Who attacked and separated the travelers ?",
+    answer: "Unknown God",
+    options: [
+      "Raiden Shogun ",
+      "Unknown God",
+      "Celestia",
+      "The Archons"
+    ]
+  },
+    {
+    numb: 7,
+    question: "Amongst the stellars, the bigger star symbolizes which twin?",
+    answer: "Aether",
+    options: [
+      "Lumine",
+      "Lyney",
+      "Aether",
+      "Mona"
+    ]
+  },
+    {
+    numb: 8,
+    question: "What is the first enemy that you encounter in game?",
+    answer: "Hydro Slime",
+    options: [
+      "Pyro Slime",
+      "Hydro Slime",
+      "Geo Slime",
+      "Anemo Slime"
+    ]
+  },
+    {
+    numb: 9,
+    question: "What reaction happens if you attack with Dendro and Hydro?",
+    answer: "Bloom",
+    options: [
+      "Bloom",
+      "Growth",
+      "Burn",
+      "Flourish"
+    ]
+  },
+    {
+    numb: 10,
+    question: "Who’s Gnosis resembles a Queen Chesspiece? ",
+    answer: "Venti",
+    options: [
+      "Venti",
+      "Nahida",
+      "Furina",
+      "Ei"
+    ]
+  },
+];
+
+
+
+
